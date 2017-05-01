dff2c1e0d11b5db777148df0c5c2148718b025d2
Daniel Adigun <daniel.adigun@digitalforte.ng>
2017-05-01 22:49:26 +0100

    implemented google+ authentication

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import { LoginService } from './login.service'
+import { AngularFire, AuthProviders, AuthMethods, FirebaseListObservable } from 'angularfire2'
 
 @Component({
     templateUrl : './login.component.html',
-    styleUrls:['./login.component.css']
+    styleUrls:['./login.component.css'],
+    //  providers:[LoginService]
 })
 
 
-export class LoginComponent{
+export class LoginComponent {
+
+  username : string; password : string;
+
+  constructor(private af : AngularFire){ }
+
+  loginWithGoogle(){
+    this.af.auth.login({
+      provider : AuthProviders.Google,
+      method: AuthMethods.Popup
+    }).then(e=>{
+      console.log('Logged in via Google');
+    })
+  }
+
+  register(){
+    this.af.auth.createUser({email: this.username, password : this.password});
+  }
+
+  logOff(){
+    this.af.auth.logout();
+  }
 
 }
