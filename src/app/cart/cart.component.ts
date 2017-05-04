e64624c3badac869a22c28ea23c655dbb514fe71
Daniel Adigun <daniel.adigun@digitalforte.ng>
2017-05-04 21:57:05 +0100

    started implementation of shopping cart

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FirebaseListObservable, AngularFire } from "angularfire2";
 
 @Component({
    selector : 'app-cart',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
 })
 
 export class CartComponent{
+
+    cartItems : FirebaseListObservable<any[]>;
+    user : any;
+
+    constructor(private af : AngularFire){
+         //get user and update cart if any
+        this.af.auth.subscribe(authState => {
+            if(authState){
+              this.cartItems =  this.af.database.list('/shoppingCart/'+authState.uid);
+            }
+        })
+    }
+
+    ngOnInit(){
+       this.getTotal();
+
+    }
+
+    getTotal(){
+        console.log(this.cartItems)
+    }
 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,9 +10,12 @@ import { AngularFire, FirebaseListObservable } from "angularfire2";
 export class HomeComponent{
     bestSellers : FirebaseListObservable<any[]>;
     newArrivals : FirebaseListObservable<any[]>;
+    user : any;
 
     constructor(private af : AngularFire){
-
+        this.af.auth.subscribe(authState =>{
+            this.user = authState.uid
+        })
     }
 
     ngOnInit(){
@@ -34,5 +37,26 @@ export class HomeComponent{
                limitToLast : 8
            }
         })
+    }
+
+    addItemToCart($key : string){
+        //get the item
+        var item;
+        this.af.database.object('/items/'+$key).subscribe(snapshot=>{
+            item = snapshot
+        })
+
+         //add item to cart
+          this.af.database.list('/shoppingCart/'+this.user)
+                .push({
+                    key : $key,
+                    name : item.title,
+                    imageURL : item.imageURL,
+                    price : item.price,
+                    quantity : 1
+                })
+                .then(x=>{alert('added item to cart')})
+                .catch(x=>{alert('unable to add item to cart')})
+       
     }
 }
